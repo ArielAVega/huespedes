1792595ef259cdf20bbe262c646ca8a8427f5711
ArielAVega <ariel.vega@educacionit.com>
2024-03-08 08:36:49 -0300

    first commit: Create project in STS

---
+++ src/test/java/ar/com/vegasystems/PvVegaArielApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.vegasystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvVegaArielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
